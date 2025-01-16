b3b28528dd6e02c86ef07b5b77bccb7f80b9dd83
Evgeniya Malashenko <malashenko_genya@mail.ru>
2025-01-16 11:45:01 +0300

    Scenario and tests are changed

--- features/step_definitions/search.steps.js
+++ features/step_definitions/search.steps.js
@@ -16,7 +16,7 @@ After(async function () {
     await this.browser.close();
   }
 });
-//общий код к сценариям находиться на странице, выбор даты и времени фильма
+
 Given('пользователь находится на странице {string}', async function (string) {
   return await this.page.goto(string);
 });
@@ -26,30 +26,9 @@ When('пользователь выбирает дату', async function () {
 When('пользователь выбирает время фильма', async function () {
   return await clickElement(this.page, ".movie-seances__time[href='#'][data-seance-id='217']");
 });
-//код к сценарию booking available ticket
-When('пользователь выбирает место', async function () {
-  return await clickElement(this.page, 'div:nth-child(3) span:nth-child(5)');
-});
-When('пользователь нажимает кнопку забронировать', async function () {
-  return await clickElement(this.page, '.acceptin-button');
-});
-Then('пользователь на странице с текстом {string}', async function (string) {
-  const actual = await getText(this.page, '.ticket__check-title');
-  const expected = string;
-  expect(actual).contain(expected);
-});
-
-//код к сценарию booking some available tickets
-
-When('пользователь выбирает место', async function () {
-  return await clickElement(this.page, 'div:nth-child(3) span:nth-child(5)');
-});
-When('Пользователь выбирает ряд {int} и место {int}', async function () {
+When('пользователь выбирает место и ряд', async function () {
   return await clickElement(this.page, 'div:nth-child(2) span:nth-child(4)');
 });
-When('Пользователь выбирает ряд {int} и место {int}', async function () {
-  return await clickElement(this.page, 'div:nth-child(5) span:nth-child(5)');
-});
 When('пользователь нажимает кнопку забронировать', async function () {
   return await clickElement(this.page, '.acceptin-button');
 });
@@ -58,8 +37,17 @@ Then('пользователь на странице с текстом {string}'
   const expected = string;
   expect(actual).contain(expected);
 });
-
-//код  к сценарию booking unavailable ticket
+When('пользователь выбирает время и зал в вип зал', async function () {
+  return await clickElement(this.page, ".movie-seances__time[href='#'][data-seance-id='198']");
+});
+When('пользователь выбирает место в вип зале', async function () {
+  return await clickElement(this.page, 'div:nth-child(7) span:nth-child(10)');
+});
+Then('пользователь видит страницу с указанной суммой {string}', async function (string) {
+  const actual = await getText(this.page, 'body main p:nth-child(6)');
+  const expected = string;
+  expect(actual).contain(expected);
+});
 When('пользователь выбирает уже забронированное место', async function () {
   return await clickElement(this.page, '.buying-scheme__chair.buying-scheme__chair_standart.buying-scheme__chair_taken');
 });
